c0d13185a5fe01b5a6d3c46eee1d602a240d066b
rup15493 <rhernandez@rayblast.com>
2023-08-09 14:16:13 -0500

    bug fix

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -2,6 +2,8 @@ import { createSelector } from 'reselect';
 import { CartState } from './cart.reducer';
 import { RootState } from '../store';
 
+
+
 const selectCartReducer = (state: RootState): CartState => state.cart;
 
 export const selectCartItems = createSelector(
